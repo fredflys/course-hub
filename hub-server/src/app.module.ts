ff7d0cc47dcca3324367edcb0098a547de9ba20f
fredflys <xyf220@126.com>
2024-06-16 18:00:06 -0700

    added and imported packages for ORM

--- hub-server/src/app.module.ts
+++ hub-server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      database: 'hub',
+      entities: [`${__dirname}/../modules/**/*.entity.ts`],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
